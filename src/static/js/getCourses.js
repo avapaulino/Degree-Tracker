809c4559fe2d7850a3375c44381c86a90c832671
paulino <av590628@dal.ca>
2021-07-28 12:39:59 -0400

    get courses code

--- src/static/js/getCourses.js
+++ src/static/js/getCourses.js
@@ -1,87 +1,36 @@
-function apiConnect() {
-    $(document).ready(function () {
-        $.ajax({
-            type: 'GET',
-            async: false,
-            url:'http://3.94.37.105:5000/grades/712744',
-            crossDomain: 'true',
-            dataType: 'json'
-        })
-    })
-}
-
-function getCourseData(event) {
-    var file = event.target.files[0];
-    if (file) {
-        var fr = new FileReader();
-        fr.onload = function (ev) {
-            var data = ev.target.result;
-            var courses = JSON.parse(data);
-
-            courses.forEach(function (course) {
-                document.getElementById('courseName').innerHTML += course.tag + " " + course.number;
-                document.getElementById('term').innerHTML += course.term + " " + course.year;
-                document.getElementById('category').innerHTML += course.category;
-                if (course.completed == "TRUE") {
-                    document.getElementById('completed').innerHTML += "Yes";
-                }
-                else {
-                    document.getElementById("completed").innerHTML += "No";
-                }
-                document.getElementById('grade').innerHTML += course.grade;
-                var info = {
-                    TableName: "Courses",
-                    Item: {
-                        "name": course.name,
-                        "number": course.number,
-                        "crn": course.crn,
-                        "tag": course.tag,
-                        "category": course.category,
-                        "completed": course.completed,
-                        "term": course.term,
-                        "year": course.year,
-                        "grade": course.grade
-                    }
-                };
-            })
-        }
+$.ajax({
+    type: 'GET',
+    async: false,
+    url:'http://3.94.37.105:5000/grades/712746',
+    crossDomain: 'true',
+    dataType: 'json'
+})
+.done(function(data) { 
+    console.log(data.body);
+    let json = JSON.parse(data.body);
+    let course = json[0];
+    if (console && console.log) {
+        console.log($('#get_courses')[0]);
     }
-}
-
-function getRequirements(event) {
-    var file = event.target.files[0];
-    if (file) {
-        var fr = new FileReader();
-        fr.onload = function (ev) {
-            var data = ev.target.result;
-            var courses = JSON.parse(data);
-
-            courses.forEach(function (course) {
-                document.getElementById('courseName').innerHTML += course.tag + " " + course.number;
-                document.getElementById('term').innerHTML += course.term + " " + course.year;
-                document.getElementById('category').innerHTML += course.category;
-                if (course.completed == "TRUE") {
-                    document.getElementById('completed').innerHTML += "Yes";
-                }
-                else {
-                    document.getElementById("completed").innerHTML += "No";
-                }
-                document.getElementById('grade').innerHTML += course.grade;
-                var info = {
-                    TableName: "Courses",
-                    Item: {
-                        "name": course.name,
-                        "number": course.number,
-                        "crn": course.crn,
-                        "tag": course.tag,
-                        "category": course.category,
-                        "completed": course.completed,
-                        "term": course.term,
-                        "year": course.year,
-                        "grade": course.grade
-                    }
-                };
-            })
+    console.log(course)
+    $.each(course, function(i) {
+        if(course[i].Completed==true){
+            var comp = "Yes";
         }
-    }
-}
+        else{
+            var comp = "No";
+        }
+        $('#get_courses').append(
+            $("<tr></tr>")
+                .append($("<td></td>").text("" + course[i].Name + ""))
+                .append($("<td></td>").text("" + course[i].Year + ""))
+                .append($("<td></td>").text("" + course[i].Term + ""))
+                .append($("<td></td>").text("" + course[i].Category + ""))
+                .append($("<td></td>").text("" + comp + ""))
+                .append($("<td></td>").text("" + course[i].Letter_Grade + ""))
+        ); 
+    });
+})
+.fail(function(){
+    alert("error");
+});
