b97f64ca14cbd66366cdd80bffba492309913cae
Takumi Kariya <tk724947@dal.ca>
2021-07-30 00:47:13 +0900

    change for url

--- src/static/js/getCourses.js
+++ src/static/js/getCourses.js
@@ -1,36 +1,35 @@
 $.ajax({
-    type: 'GET',
-    async: false,
-    url:'http://3.94.37.105:5000/grades/712746',
-    crossDomain: 'true',
-    dataType: 'json'
-})
-.done(function(data) { 
-    //console.log(data.body);
-    let json = JSON.parse(data.body);
-    let course = json[0].Courses_taken;
-    
-    $('#name').text(json[0].Name);
-    console.log(json[0].Name);
+        type: 'GET',
+        async: false,
+        url: 'http://3.94.37.105:5000/grades/712746',
+        crossDomain: 'true',
+        dataType: 'json'
+    })
+    .done(function(data) {
+        //console.log(data.body);
+        let json = JSON.parse(data.body);
+        let course = json[0].Courses_taken;
 
-    $.each(course, function(i) {
-        if(course[i].Completed==true){
-            var comp = "Yes";
-        }
-        else{
-            var comp = "No";
-        }
-        $('#get_courses').append(
-            $("<tr></tr>")
+        $('#name').text(json[0].Name);
+        console.log(json[0].Name);
+
+        $.each(course, function(i) {
+            if (course[i].Completed == true) {
+                var comp = "Yes";
+            } else {
+                var comp = "No";
+            }
+            $('#get_courses').append(
+                $("<tr></tr>")
                 .append($("<td></td>").text("" + course[i].Name + ""))
                 .append($("<td></td>").text("" + course[i].Year + ""))
                 .append($("<td></td>").text("" + course[i].Term + ""))
                 .append($("<td></td>").text("" + course[i].Category + ""))
                 .append($("<td></td>").text("" + comp + ""))
                 .append($("<td></td>").text("" + course[i].Letter_Grade + ""))
-        ); 
+            );
+        });
+    })
+    .fail(function() {
+        alert("error");
     });
-})
-.fail(function(){
-    alert("error");
-});
